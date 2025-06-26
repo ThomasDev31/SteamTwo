b1653cab1cda8238b1b3b1e139d396b4a0db9538
ThomasDev31 <thomas.tissier@3wa.io>
2025-06-26 14:06:17 +0200

    purification du code

--- Front/src/components/little_components/Game.jsx
+++ Front/src/components/little_components/Game.jsx
@@ -1,52 +1,59 @@
 import { useEffect, useState } from "react";
-import{useNavigate, useParams} from "react-router";
+import { useNavigate, useParams } from "react-router";
 import rawgCalls from "../../api/rawgCalls";
 import styled from "styled-components";
-import { act } from "react";
-function Game(){
-    const[datas, setDatas] = useState(null);
-    const[error, setError] = useState('');
-    const[loading, setLoading] = useState(true);
-    const {id} = useParams()
-    const navigate = useNavigate(); 
 
-    const fetchData = async()=>{
-         if(id){
+function Game() {
+    const [datas, setDatas] = useState(null);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const fetchData = async () => {
+        if (id) {
             try {
-                setLoading(true)
+                setLoading(true);
                 const responses = await rawgCalls.getGame(id);
-                if(responses.error != null){
-                    setError(responses.error)
-                }else{
-                    setDatas(responses[0].result)
+                if (responses.error != null) {
+                    setError(responses.error);
+                } else {
+                    setDatas(responses[0].result);
                 }
-                
             } catch (err) {
-                setError(err.message)
-            }finally{
-                setLoading(false)
+                setError(err.message);
+            } finally {
+                setLoading(false);
             }
         }
-    }
+    };
     useEffect(() => {
-       fetchData();
-    },[id])
-  
-    return (<>{datas && <StyledModal >{ console.log(datas)}
-    
-            <button onClick={()=> navigate('/') }>Retirer</button>
-        </StyledModal>}</>)
+        fetchData();
+    }, [id]);
+
+    if (error) return <div>{error}</div>;
+    if (loading) return <div>Loading ...</div>;
+
+    return (
+        <>
+            <StyledModal>
+                {error && <div>{error}</div>}
+                {loading && <div><h2>Loading ...</h2></div>}
+                {!error && !loading && datas && (
+                    <button onClick={() => navigate("/")}>Retirer</button>
+                )}
+            </StyledModal>
+        </>
+    );
 }
 
-
-
 const StyledModal = styled.div`
-    max-width:1240px;
-    margin:auto;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    height:100vh;
+    max-width: 1240px;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
     position: relative;
 `;
 export default Game;

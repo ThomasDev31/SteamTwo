0dc29d44b3e0916220a61204cf746d0c3ee71b37
ThomasDev31 <thomas.tissier@3wa.io>
2025-06-27 12:18:50 +0200

    nothing

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./layout/Layout";
 import Game from "./components/little_components/Game";
 import Header from "./components/Header";
 import Cart from "./components/little_components/Cart";
+import Error404 from "./layout/Error404"
 import { CartProvider } from "./components/contexts/CartContext";
 import { BrowserRouter, Routes, Route } from "react-router";
 function App() {
@@ -14,6 +15,8 @@ function App() {
 					<Routes>
 						<Route path="/" element={<Layout />} />
 						<Route path="/game/:id" element={<Game />} />
+						<Route path="*" element={<Error404 />} />
+						
 					</Routes>
 				</CartProvider>
 			</BrowserRouter>
--- Front/src/layout/Error404.jsx
+++ Front/src/layout/Error404.jsx
@@ -1,4 +1,95 @@
-function Error404(){
-    return (<> <p>Error 404</p></>);
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
+
+function Error404() {
+    const [active, setActive] = useState(false);
+    const navigate = useNavigate();
+    return (
+        <>
+            <StyledError>
+                <div className="container-error">
+                    <h1>Error 404</h1>
+                    <p
+                        onClick={() => {
+                            setActive(true);
+                            setTimeout(() => {
+                                setActive(false);
+                                navigate('/');
+                            }, 3000)
+                        }}
+                    >
+                        retourner à l'accueil
+                    </p>
+                </div>
+                <div className={`container-modal ${active ? "active" : ""}`}>
+                    <div>
+                        <h1>Hello petit coquin</h1>
+                        <img src="../src\assets\2112.gif" alt="" />
+                    </div>
+                </div>
+            </StyledError>
+        </>
+    );
 }
+
+const StyledError = styled.div`
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    .container-error {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        gap: 30px;
+        color: whitesmoke;
+        h1 {
+            font-size: 3rem;
+        }
+        p {
+            color: pink;
+            font-size: 1.5rem;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    }
+    .container-modal {
+        border: 1px solid white;
+        width: 50%;
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50%;
+        margin: 0;
+        z-index: -100;
+        opacity:0;
+        background-color: rgba(255, 255, 255, 0.1);
+        border-radius: 20px;
+        div {
+            background-color: white;
+            height: 80%;
+            width: 80%;
+            border-radius: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            img{
+                width:100%;
+                border-radius:20px;
+            }
+            h1{
+                position:absolute;
+                bottom:20px;
+            }
+        }
+        &.active{
+            opacity:1;
+            z-index:100;
+        }
+    }
+    
+`;
 export default Error404;
